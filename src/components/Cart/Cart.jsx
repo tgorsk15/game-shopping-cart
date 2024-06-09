cb92ab37f68d3a9e883551f90db74a4b92f46cb3
tgorsk15 <tgbusiness463@gmail.com>
2024-06-09 16:32:02 -0500

    Add styling and imgs to itemsContainer section

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,4 +1,5 @@
 import { useOutletContext } from "react-router-dom"
+import cartStyles from './Cart.module.css'
 
 
 export const Cart = () => {
@@ -8,11 +9,42 @@ export const Cart = () => {
 
 console.log(shoppingCart)
 
+// create a test file that ensures the Order Total Calculation 
+// is being done in the right way
+
+// 6/9/24: After I am done building skeleton of Cart, I should write an
+// implementation test that checks if items are added to cart on Shop page
+
     return (
-        <main>
-            <div>
-                I am the cart
-            </div>
+        <main className={cartStyles.cartPage}>
+            {shoppingCart.length > 0 ? (
+                <section className={cartStyles.itemsSection}>
+                    {shoppingCart.map((item) => {
+                        {console.log(item)}
+                        return (
+                            <div className={cartStyles.itemContainer} key={item.id}>
+                                <div 
+                                    className={cartStyles.gameImage}
+                                    style={{backgroundImage: `url(${item.background_image})`}}
+                                >
+                                </div>
+                            </div>
+                        )
+                    })
+
+                    }
+                </section>
+            ) : (
+                <h1> you no items in your cart currently</h1>
+            )}
+            
+            <aside className={cartStyles.checkoutSection}>
+                <h2 className={cartStyles.orderSummary}>Your Order:</h2>
+                <div className={cartStyles.orderContainer}>
+                    {/* will have to set up a totalCost state to
+                    show order details here */}
+                </div>
+            </aside>
         </main>
     )
 }
