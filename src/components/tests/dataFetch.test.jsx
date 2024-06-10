60e937bc99071b7c68edecc6340d9616e49f4369
tgorsk15 <tgbusiness463@gmail.com>
2024-06-10 18:09:35 -0500

    Comment out API fail test

--- src/components/tests/dataFetch.test.jsx
+++ src/components/tests/dataFetch.test.jsx
@@ -1,4 +1,4 @@
-import { describe, expect, it, test, vi } from 'vitest'
+import { afterEach, describe, expect, it, test, vi } from 'vitest'
 import { render, screen, waitForElementToBeRemoved } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
@@ -6,19 +6,26 @@ import { routes } from '../routes';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 
-window.fetch = vi.fn(() => {
-    const user = { name: 'Albus', phone:'630-888-7755'};
 
-    return Promise.resolve({
-        json: () => Promise.resolve(user)
-    })
-    
-})
 
 describe('API fetch', () => {
+
+    afterEach(() => {
+        vi.resetAllMocks(); // Reset mocks after each test
+    });
+
     const router = createBrowserRouter(routes)
 
     it('loads "loading" screen while API is being fetched', async () => {
+        window.fetch = vi.fn(() => {
+            const user = { name: 'Albus', phone:'630-888-7755'};
+
+            return Promise.resolve({
+                json: () => Promise.resolve(user)
+            })
+            
+        })
+
         render(<RouterProvider router={router} />);
 
         screen.debug()
@@ -28,4 +35,26 @@ describe('API fetch', () => {
 
         await waitForElementToBeRemoved(() => screen.getByText('Loading ...'))
     })
+
+    // it('produces error message when fetch fails', async () => {
+    //     // window.fetch.mockImplementationOnce(() => {
+    //     //     return Promise.reject({message: 'API is down'});
+    //     // });
+
+    //     window.fetch = vi.fn(() => {
+    //         return Promise.reject({
+    //             message: 'API is down'
+    //         })
+            
+    //     })
+
+    //     render(<RouterProvider router={router} />)
+
+    //     screen.debug()
+
+    //     const errorMessage = await screen.findByText('API is down');
+    //     expect(errorMessage).toBeInTheDocument()
+
+    //     screen.debug()
+    // })
 })
