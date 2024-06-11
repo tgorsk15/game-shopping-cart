ea341a5972dbc8139aec6fa70c44655d8ad429c5
tgorsk15 <tgbusiness463@gmail.com>
2024-06-10 20:04:29 -0500

    Add mock router to ShopPage test

--- src/components/tests/ShopPage.test.jsx
+++ src/components/tests/ShopPage.test.jsx
@@ -1,8 +1,10 @@
 import { afterEach, describe, expect, it, test, vi } from 'vitest'
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { MemoryRouter, Routes, Route, Outlet } from 'react-router-dom';
 
 import { ShopPage } from '../ShopPage/ShopPage';
+import { App } from '../../App';
 
 describe("Shop Page", () => {
     // test to make sure item gets added to cart
@@ -12,10 +14,29 @@ describe("Shop Page", () => {
         const handleAddItem = () => {
             console.log('added')
         }
+        const gameData = [{ id: 1, name: 'Game 1' }];
+        const shoppingCart = [];
 
         const user = userEvent.setup()
 
-        render(<ShopPage handleAddItem={handleAddItem} />)
+        // render(<ShopPage handleAddItem={handleAddItem} />)
+
+        render(
+            <MemoryRouter initialEntries={['/shop']}>
+                <Routes>
+                    <Route path="/" element={<App />}>
+                        <Route path="shop" element={<ShopPage />} />
+                    </Route>
+                </Routes>
+            </MemoryRouter>,
+            {
+                wrapper: ({ children }) => (
+                    <Outlet context={{ gameData, shoppingCart, handleAddItem }}>
+                        {children}
+                    </Outlet>
+                ),
+            }
+        );
 
         const addButton = screen.getAllByRole("button", {name: "Add to Cart"})
         console.log(addButton)
