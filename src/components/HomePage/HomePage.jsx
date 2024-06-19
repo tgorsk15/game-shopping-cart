5063e8e6c9b5448fa84be136e19033762939dc03
tgorsk15 <tgbusiness463@gmail.com>
2024-06-19 12:54:05 -0500

    Develop bottomSection and create new Shop Link

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
 import homeStyles from './HomePage.module.css'
 
+import { Link } from "react-router-dom"
+
 export const HomePage = () => {
 
 
@@ -8,13 +10,28 @@ export const HomePage = () => {
         <main className={homeStyles.homePage}>
             <div className={homeStyles.homeContainer}>
                 <div className={homeStyles.topSection}>
-                    {/* give this container a background image */}
                     <h1 className={homeStyles.homeSlogan}>
-                        Find your inner Gamer
+                        Find your Inner Gamer
                     </h1>
                 </div>
                 <div className={homeStyles.bottomSection}>
-
+                    <h2 className={homeStyles.bottomTitle}>
+                        Welcome to one of the world's largest Video Game databases...
+                    </h2>
+                    <p className={homeStyles.bottomParagraph}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
+                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+                        Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea.
+                    </p>
+                    <div className={homeStyles.shopBtnContainer}>
+                        <Link 
+                            to="../shop" 
+                            className={homeStyles.shopBtn}
+                        >
+                            Start Shopping
+                        </Link>
+                    </div>
                 </div>
             </div>
         </main>
